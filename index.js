52f0f01910521825f421e652897450bd23ad7526
Ifechi <32471036+iamifechi@users.noreply.github.com>
2023-01-15 19:58:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+function validateCard(cardNum, cardType) {
+  //remove all space and non-digit charcters from the card number
+  //since credit cards are strictly numbers only
+  cardNum = cardNum.replace(/\D/g, "");
+
+
+  // Define regular expressions for each card type
+  let regexes = {
+    //Visa cards are usually 13 or 16 digits long
+    //starts with 4.
+    // If the cards have 13 digits the next twelve digits would be any number between 0-9.
+    // If the cards have 16 digits the next fifteen digits would be any number between 0-9.
+    Visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+
+    //Mastercards are usually 16 digits long.
+    //starts two digits numbers ranging from 51 to 55 or four digits numbers rangin from 2221 to 2720.
+    // if the range is 51 to 55, the remaining fourteen digits should be any number between 0-9.
+    //but if the range is 2221 to 2720, the remaining twelve digits should be any number between 0-9.
+    Mastercard: /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/
+  };
+
+  // Check if the card number matches the regex for the given card type
+  let regex = regexes[cardType];
+  if (!regex) {
+    console.log("Invalid card type: " + cardType);
+    return false;
+  }
+
+  let isValid = cardNum.match(regex);
+
+  return isValid !== null;
+}
+
+//Test case 1 outputs true
+console.log(validateCard('4155279860457', 'Visa'))
+
+//Test case 2 outputs false
+console.log(validateCard('4155 2798 604579', 'Visa'))
+
+//Test case 3 outputs true
+console.log(validateCard('5155 2798 6045 7678', 'Mastercard'))
+
+//Test case 4 outputs true
+console.log(validateCard('2255 2798 6045 7678', 'Mastercard'))
+
+//Test case 5 outputs false
+console.log(validateCard('2155 2798 6045 7678', 'Mastercard'))
